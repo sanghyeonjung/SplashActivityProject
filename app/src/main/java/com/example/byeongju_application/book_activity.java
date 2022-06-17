48025b88ee258d21b4444cef112b2319de95b8be
sanghyeonjung <roy1109@naver.com>
2022-06-17 22:32:45 +0900

    message

---
+++ app/src/main/java/com/example/byeongju_application/book_activity.java
@@ -0,0 +1,13 @@
+package com.example.byeongju_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class book_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book);
+    }
+}
